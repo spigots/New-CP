7391dc1b216441c7b086d2abc14569040ef9a103
spigots <kirk.ibiz@gmail.com>
2011-09-10 16:25:37 -0600

    .

--- sample-client.php
+++ sample-client.php
@@ -16,13 +16,13 @@
 				</div>	
 				<div class="flat_area grid_10">
 					<h2>Gustafason Roofing</h2>
-					<div class="flat_area grid_5">
-					<button style="float: left;" class="orange skin_colour round_all clearfix"><img src="images/icons/small/white/Pacman.png" height="24"><span>Extended Marketing</span></button>
-					<button style="float: right;" class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
+					<div class="grid_5">
+					<button class="orange skin_colour round_all"><img src="images/icons/small/white/Pacman.png" height="24"><span>Extended Marketing</span></button>
+					<button class="skin_colour round_all"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
 					<button class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
 					<button class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
 					</div>
-					<div class="flat_area grid_5">
+					<div class="grid_5">
 					<button class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
 					<button class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
 					<button class="skin_colour round_all clearfix"><img src="images/icons/small/white/Globe%202.png" height="24"><span>Maps listing: Boise, ID</span></button>
